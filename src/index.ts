9eda00931157ea905dc419a9823e2097975728d0
jarikoff <v.zharikov@prizyvanet.ru>
2021-11-24 14:17:36 +0600

    (WIP) Implement http server up

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-// TODO add Client consumer and start it work, create an http server to listen rabbit and get potential requests via http
+import * as express from 'express';
+import * as http from 'http';
+import { Logger } from './logger/logger';
+
+const port = 3050;
+const logger: Logger = new Logger('index');
+
+process.on('uncaughtException', (err) => {
+    logger.error('Caught uncaught exception', err);
+});
+
+const app = express();
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port);
+
+server.on('listening', (): void => {
+    const address = server.address();
+    const bind = (typeof address === 'string') ? `pipe ${ address }` : `port ${ address.port }`;
+    logger.info(`Listening on ${ bind }`);
+});
+
